9507c0151e78fe8bc97754bac17c0603521b437f
Arfath <mhdarfath868@gmail.com>
2026-02-12 12:08:53 +0530

    url change

--- src/components/Constants/Constants.tsx
+++ src/components/Constants/Constants.tsx
@@ -1,2 +1,2 @@
-// export const backendUrl = 'https://www.cipher-peak.com/';
-export const backendUrl = 'http://127.0.0.1:8000/';
+export const backendUrl = 'https://www.cipher-peak.com/';
+// export const backendUrl = 'http://127.0.0.1:8000/';
